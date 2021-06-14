6624729c821f30642cb2cfe3ff23defa14a84977
camilemuz <lemuzmujiquita@gmail.com>
2021-06-14 15:21:10 -0400

    edita municipio

--- src/app/components/configuracion/lugar/municipio/municipio.component.ts
+++ src/app/components/configuracion/lugar/municipio/municipio.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Departamento } from 'src/app/models/departamento.model';
 import { Municipio } from 'src/app/models/municipio.model';
 import { SolicitudService } from 'src/app/services/solicitud.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-municipio',
@@ -10,30 +14,95 @@ import { SolicitudService } from 'src/app/services/solicitud.service';
 })
 export class MunicipioComponent implements OnInit {
   public municipios: Municipio[] = [];
+  public municipio: Municipio;
+  public submitted=false;
+  public form: FormGroup;
+
   constructor(
     private parametroService:SolicitudService,
-    private router:Router
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
-    this.parametroService.municipios().subscribe((resp: any) => {
+    this.index();
+    this.form = this.formBuilder.group({
+      lugar: [null, Validators.required],
+      cod: [null, Validators.required],
+    
+    });
+  }
+ 
+  get f(){
+    return this.form.controls;
+  }
+
+  private index(){
+    let cust = {
+      email: localStorage.getItem('usuario'),
+      token: localStorage.getItem('token')
+    }
+     this.parametroService.indexmunicipio(cust).subscribe((resp: any) => {
       if (resp.respuesta){
         this.municipios = resp.municipios;
+        
+        
       }
     });
   }
-  editUser(row:any,id: number) {
-    // console.log('sss',row);
-    
 
-     this.router.navigate(['/editar-usuario',id]);
-// this.auth.editarUsuario(row,id).subscribe(show=>
-//   {
-//     console.log(show);
-    
-//   })
-  }
+  private accionEditar(content: any,municipio:Municipio){
+
+    this.municipio=new Municipio();
+    this.municipio=municipio;
+    this.modalService.open(content,{size:'lg'});
 
   }
 
+  public registrar(){
+    this.submitted=true;
+    if(this.form.invalid){
+      Swal.fire(
+        'Falta',
+        'Tiene marcados los campos que faltan',
+        'warning'
+      );
+      return;
+    }
+    
+    this.parametroService.editarmunicipio(this.municipio.id_lugar,this.municipio).subscribe((resp:any)=>{
+      if(resp.respuesta){
+        this.modalService.dismissAll();
+        this.index();
+        
+        
+      }
+    })
+    
+  }
 
+
+  public eliminar(municipio: Municipio){
+    console.log(municipio);
+    Swal.fire({
+      title: '¿Está seguro de eliminar el municipio?',
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: `Eliminar`,
+      denyButtonText: `Cancelar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        this.parametroService.eliminarmunicipio(municipio).subscribe((resp: any) => {
+          if (resp.respuesta){
+            Swal.fire(resp.mensaje, '', 'success');
+            this.index();
+          }else{
+            Swal.fire('Cancelado', '', 'info')
+          }
+        })
+      }
+    })
+  }
+}
+  
